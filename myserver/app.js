65c696e21cff05bd67315cfe2177c87654bea63b
iamkda-q <90349920+iamkda-q@users.noreply.github.com>
2022-04-13 20:18:25 +0300

    Update app.js

--- myserver/app.js
+++ myserver/app.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const userRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
 
-const { PORT = 3000 } = process.env;
+const PORT = 3000;
+
+//const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
