759a67c7d358c1e3e43416690de4a864c274d2aa
KDA <iamkda@yandex.ru>
2022-05-12 23:32:04 +0300

    fix: refactoring

--- app.js
+++ app.js
@@ -2,91 +2,36 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
 const mongoose = require("mongoose");
-const { errors, celebrate, Joi } = require("celebrate");
-const userRouter = require("./routes/users");
-const cardRouter = require("./routes/cards");
-const { createUser, login } = require("./controllers/users");
-const auth = require("./middlewares/auth");
-const { NotFoundError } = require("./errors/errors");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const router = require("./routes/router");
+const errorHandler = require("./middlewares/errorHandler");
 
 const PORT = 3000;
 
 const app = express();
 
-/* const allowedCors = [
-    "mydomain.mesto.nomoredomains.xyz",
-    "localhost:3000",
-];
-
-app.use((req, res, next) => {
-    const { origin } = req.headers;
-    if (allowedCors.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-    const requestHeaders = req.headers["access-control-request-headers"];
-    const { method } = req;
-    if (method === "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", requestHeaders);
-    }
-    next();
-});
- */
-
 /* CORS */
 app.use(cors());
 app.use(helmet());
 
-/* Парсер из JSON */
-app.use(bodyParser.json());
-
 /* мидлвара логирования запросов */
 app.use(requestLogger);
 
-app.post("/signin", celebrate({
-    body: Joi.object().keys({
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-    }),
-}), login);
+/* Парсер из JSON */
+app.use(bodyParser.json());
 
-app.post("/api/signup", celebrate({
-    body: Joi.object().keys({
-        name: Joi.string().min(2).max(30),
-        about: Joi.string().min(2).max(30),
-        // eslint-disable-next-line no-useless-escape
-        avatar: Joi.string().regex(/^https?:\/\/(www\.)?([a-zA-Z0-9]([a-zA-Z0-9-]+[a-zA-Z0-9])|([a-zA-Z0-9]*)\.)+[a-zA-Z]{2,}\/[\w\.\+@:_'~,-=#;\!\&\[\]\/\$\|\?\*\(\)]+$/),
-        email: Joi.string().required().email(),
-        password: Joi.string().required(),
-    }),
-}), createUser);
-
-app.use(auth, celebrate({
-    headers: Joi.object().keys({
-        authorization: Joi.string().required().regex(/Bearer/),
-    }).unknown(true),
-}));
-
-app.use("/", cardRouter);
-app.use("/", userRouter);
-
-app.use("/", () => {
-    throw new NotFoundError("Такой страницы не существует");
-});
+app.use("/api", router);
 
 /* мидлвара логирования ошибок */
 app.use(errorLogger);
 
+/* мидлвара централизованной обработки ошибок Celebrate */
 app.use(errors());
 
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-    const { statusCode = 500, message } = err;
-    res.status(statusCode).send({
-        message: statusCode === 500 ? "На сервере произошла неизвестная ошибка" : message,
-    });
-});
+/* мидлвара на остальные ошибки */
+app.use(errorHandler);
 
 mongoose
     .connect("mongodb://localhost:27017/mestodb", {
---
+++ middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+const errorHandler = require("express").Router();
+const { NotFoundError } = require("../errors/errors");
+
+errorHandler.use("/", () => {
+    throw new NotFoundError("Такой страницы не существует");
+});
+
+// eslint-disable-next-line no-unused-vars
+errorHandler.use((err, req, res, next) => {
+    const { statusCode = 500, message } = err;
+    res.status(statusCode).send({
+        message: statusCode === 500 ? "На сервере произошла неизвестная ошибка" : message,
+    });
+});
+
+module.exports = errorHandler;
