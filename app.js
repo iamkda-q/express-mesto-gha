0c62644ba33319880d0063e9356bc659a8c88aea
KDA <iamkda@yandex.ru>
2022-05-12 23:58:50 +0300

    fix: refactor errorsHandle

--- app.js
+++ app.js
@@ -32,7 +32,13 @@ app.use(errors());
 
 /* мидлвара на остальные ошибки */
 app.use(errorHandler);
-
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message } = err;
+    res.status(statusCode).send({
+        message: statusCode === 500 ? "На сервере произошла неизвестная ошибка" : message,
+    });
+});
 mongoose
     .connect("mongodb://localhost:27017/mestodb", {
         useNewUrlParser: true,
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -5,12 +5,4 @@ errorHandler.use("/", () => {
     throw new NotFoundError("Такой страницы не существует");
 });
 
-// eslint-disable-next-line no-unused-vars
-errorHandler.use((err, req, res, next) => {
-    const { statusCode = 500, message } = err;
-    res.status(statusCode).send({
-        message: statusCode === 500 ? "На сервере произошла неизвестная ошибка" : message,
-    });
-});
-
 module.exports = errorHandler;
